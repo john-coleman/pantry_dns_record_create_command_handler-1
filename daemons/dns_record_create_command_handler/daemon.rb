e974dbd445d3415c2fec17f430293355fba172e8
John Coleman <john.coleman@wonga.com>
2013-08-01 15:12:47 +0100

    Fixing typo in dns_record_create_command_handler daemon.rb which we don't test. Need to move to the metaprogramming way & light

--- daemons/dns_record_create_command_handler/daemon.rb
+++ daemons/dns_record_create_command_handler/daemon.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'daemons'
 require_relative '../common/subscriber'
 require_relative '../common/config'
-require_relative 'dns_create_record_command_handler'
+require_relative 'dns_record_create_command_handler'
 
 THIS_FILE = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
 config = Daemons::Config.new(File.expand_path(File.join(File.dirname(THIS_FILE),"daemon.yml")))
@@ -15,7 +15,7 @@ daemon_config = {
 }
 Daemons.run_proc(config['daemon']['app_name'], daemon_config) {
   begin
-    Daemons::Subscriber.new.subscribe(config['sqs']['queue_name'],Daemons::DnsCreateRecordCommandHandler.new(config['sns']['topic_arn']))
+    Daemons::Subscriber.new.subscribe(config['sqs']['queue_name'],Daemons::DnsRecordCreateCommandHandler.new(config['sns']['topic_arn']))
   rescue => e
     puts "#{e}"
     retry
